9f0653787bcf1948117a088b48f0ac6cf9ec3ebd
jianfeng <hjfeng1713@163.com>
2025-07-21 18:37:19 +0800

    1

---
+++ README.md
@@ -0,0 +1,9 @@
+# 项目介绍
+
+* 直接打开index.html会提示“无法加载导航数据”，因为本地file://协议有限制，不能跨文件读取sites.json
+* 需要使用Web服务器，如nginx、vscode live-server等
+* 只需要修改sites.json
+
+![效果截图](1.png)
+
+![搜索效果](2.png)
---
+++ main.js
@@ -0,0 +1,48 @@
+let allData = [];
+const nav = document.getElementById('nav');
+const search = document.getElementById('search');
+
+function renderNav(data) {
+  nav.innerHTML = data
+    .filter(group => group.sites && group.sites.length)
+    .map(group => `
+      <div class="site-group">
+        <h2 class="group-title">${group.group}</h2>
+        <div class="group-sites">
+          ${group.sites.map(site =>
+            site.enable !== false && site.url
+              ? `<a class="site-item" href="${site.url}" target="_blank">
+                  <span class="site-name">${site.name}</span>
+                  ${site.desc ? `<span class="desc">${site.desc}</span>` : ''}
+                </a>`
+              : `<div class="site-item disabled-site">
+                  <span class="site-name">${site.name}</span>
+                  ${site.desc ? `<span class="desc">${site.desc}</span>` : ''}
+                </div>`
+          ).join('')}
+        </div>
+      </div>`
+    ).join('');
+}
+
+fetch('sites.json')
+  .then(res => res.json())
+  .then(data => {
+    allData = data;
+    renderNav(allData);
+  })
+  .catch(() => {
+    nav.innerHTML = '<p>无法加载导航数据</p>';
+  });
+
+search.addEventListener('input', function() {
+  const keyword = this.value.trim().toLowerCase();
+  if (!keyword) return renderNav(allData);
+  const filtered = allData
+    .map(group => ({
+      ...group,
+      sites: group.sites.filter(site => site.name.toLowerCase().includes(keyword))
+    }))
+    .filter(group => group.sites.length);
+  renderNav(filtered);
+});
